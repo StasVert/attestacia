24c199591a09d65b43c32505c29f59e394562a89
Stanislav_Vertkin <s.vertkin@gmail.com>
2024-05-02 19:48:57 +0300

    task 3

---
+++ task 3/Program.cs
@@ -0,0 +1,16 @@
+﻿//3. Задайте произвольный массив. Выведите его элементы, начиная с конца. Использовать рекурсию.
+
+int[] MyArray = {1, 5, 9, 10};
+
+// for(int i = myArray.Length - 1; i >= 0; i--)
+
+void PrintArray(int[] MyArray, int i = 3)
+{
+    if (i >= 0)
+    {
+       System.Console.WriteLine(MyArray[i]);
+       PrintArray(MyArray, i - 1);
+    }
+}
+
+PrintArray(MyArray);
